e8f8902afaab73576dbac9cf0f7a4fd41a4ee18e
Ahmed Adel <69484554+Ahmed23Adel@users.noreply.github.com>
2024-03-07 18:40:44 +0200

    Adding input validators with animation

--- FrontEnd.vue/Rowling/src/views/LoginView.vue
+++ FrontEnd.vue/Rowling/src/views/LoginView.vue
@@ -4,6 +4,39 @@ import { ref } from 'vue';
 
 const usernameValue = ref("")
 const passwordValue = ref("")
+const isUsernameValid = ref(true)
+const isPasswordValid = ref(true)
+
+
+function login() {
+    if (validateInput()) {
+
+    }
+}
+
+function validateInput() {
+    const username = usernameValue.value.trim();
+    const password = passwordValue.value.trim();
+
+    if (username.length < 4) {
+        isUsernameValid.value = false;
+    }
+    else{
+        isUsernameValid.value = true;
+    }
+    if (password.length < 4) {
+        isPasswordValid.value = false;
+    }
+    else{
+        isPasswordValid.value = true;
+    }
+    if (username.length < 4 || password.length < 4) {
+        return false;
+    }
+    return true;
+}
+
+
 </script>
 <template>
     <div class="div-background">
@@ -15,7 +48,7 @@ const passwordValue = ref("")
             <h3>This is where all plants magic take place</h3>
         </header>
 
-        <div class="usernamePassword-div">
+        <div class="usernamePassword-div" id="username">
             <div class="card flex justify-content-center">
                 <FloatLabel>
                     <InputText id="username" v-model="usernameValue" />
@@ -24,15 +57,30 @@ const passwordValue = ref("")
             </div>
 
         </div>
+        <Transition>
+            <div class="alert-container" v-if="!isUsernameValid" @click="isUsernameValid = true">
+                <div class="alert alert-error">❌ Username must be at least 4 characters..❌</div>
 
-        <div class="usernamePassword-div">
+            </div>
+        </Transition>
+
+
+        <div class="usernamePassword-div" id="password">
             <FloatLabel>
                 <Password v-model="passwordValue" toggleMask :feedback="false" />
                 <label for="password">Password</label>
             </FloatLabel>
         </div>
 
-        <div class="button-container-2">
+        <Transition>
+            <div class="alert-container" v-if="!isPasswordValid" @click="isPasswordValid = true">
+                <div class="alert alert-error">❌Password must be at least 4 characters..❌</div>
+
+            </div>
+        </Transition>
+
+
+        <div class="button-container-2" @click="login" id="submit">
             <span class="mas">Submit</span>
             <button type="button" name="Hover">Submit</button>
         </div>
