7f08cf222f1ba163f3951ff59a4527b1803a4254
Ahmed Adel <69484554+Ahmed23Adel@users.noreply.github.com>
2024-03-07 17:14:05 +0200

    Design of Login page

--- FrontEnd.vue/Rowling/src/main.ts
+++ FrontEnd.vue/Rowling/src/main.ts
@@ -1,8 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-const app = createApp(App);
+import 'primevue/resources/themes/lara-dark-teal/theme.css'
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext'
+import FloatLabel from 'primevue/floatlabel';
+import Password from 'primevue/password';
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const app = createApp(App, { ripple: true });
+
 app.use(router)
+app.use(ElementPlus)
+app.use(PrimeVue);
+app.component('InputText', InputText)
+app.component('FloatLabel', FloatLabel)
+app.component('Password', Password)
 
 app.mount('#app')
--- FrontEnd.vue/Rowling/src/views/LoginView.vue
+++ FrontEnd.vue/Rowling/src/views/LoginView.vue
@@ -1,5 +1,47 @@
+<script setup>
+import InputText from 'primevue/inputtext';
+import { ref } from 'vue';
+
+const usernameValue = ref("")
+const passwordValue = ref("")
+</script>
 <template>
-    <div>
-        Hello World
+    <div class="div-background">
+
     </div>
+    <div class="content">
+        <header>
+            <h1>Welcome to Rowling (Login)</h1>
+            <h3>This is where all plants magic take place</h3>
+        </header>
+
+        <div class="usernamePassword-div">
+            <div class="card flex justify-content-center">
+                <FloatLabel>
+                    <InputText id="username" v-model="usernameValue" />
+                    <label for="username">Username</label>
+                </FloatLabel>
+            </div>
+
+        </div>
+
+        <div class="usernamePassword-div">
+            <FloatLabel>
+                <Password v-model="passwordValue" toggleMask :feedback="false" />
+                <label for="password">Password</label>
+            </FloatLabel>
+        </div>
+
+        <div class="button-container-2">
+            <span class="mas">Submit</span>
+            <button type="button" name="Hover">Submit</button>
+        </div>
+
+    </div>
+
 </template>
+
+
+<style lang="scss" scoped>
+@import "../assets/scss/LoginView.scss";
+</style>
